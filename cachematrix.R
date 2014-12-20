25620d91b1ab4c98d7bd7edd91285dca489eec81
sachin959 <sgautam959@gmail.com>
2014-12-20 20:07:57 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+## this functions here caches the Inverse of a matrix which can be used later without recomputing the 
+## the inverse again
+## makeCacheMatrix contains is the core function and contain four subfunctions
+## set : to set the data for calculating the inverse
+## get : retrieve the matrix whose inverse is calculated i.e. original matrix
+## getInv : getthe cached Inverse Matrix
+## setInv : cache the Calculated Inv for later use
 
-## Write a short comment describing this function
+## At first Inverse Matrix is null
+## later on first retrieval value is set.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    res <- NULL
+    set <- function(y){
+        x <<- y
+        res <<- NULL
+    }
+    get <- function() x
+    getInv <- function() res
+    setInv <- function(z){
+        res <<- z
+    }
+    list(set = set,
+         get = get,
+         getInv = getInv,
+         setInv = setInv)
 }
 
 
@@ -12,4 +31,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        mInv <- x$getInv()
+    ## if mInv is not null than the cached matrix is returned
+    if(!is.null(mInv)){ 
+        print("getting Cached Matrix");
+        mInv
+    }
+    ## if mInv is null Inverse has not yet been calculated so first
+    ## its calculated and then this matrix is returned
+    else{
+        z <- x$get();
+        z <- solve(z);
+        x$setInv(z);
+        z
+    } 
 }
